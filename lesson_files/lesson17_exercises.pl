71369af88f5e97fabe5a3340729d9d286a2ead6a
martinghunt <martin.g.hunt@gmail.com>
2017-03-10 23:35:39 +0000

    Simplify references section and exercises

--- lesson_files/lesson17_exercises.pl
+++ lesson_files/lesson17_exercises.pl
@@ -6,34 +6,46 @@ use warnings;
 
 use Data::Dumper;
 
-my %codes;
-$codes{'earth'} = {
-    'serine'  => [ 'TCA', 'TCC', 'TCG', 'TCT' ],
-    'proline' => [ 'CCA', 'CCC', 'CCG', 'CCT' ],
-};
-$codes{'mars'} = {
-    'serine'  => [ 'QWZ', 'QWX', 'QWQ', 'QWW' ],
-    'proline' => [ 'ZXZ', 'ZXX', 'ZXQ', 'ZXW' ],
-};
 
-my @code_bases = get_code_bases(\%codes);
-print Dumper \@code_bases;
-
-sub get_code_bases {
-    my ($code_ref) = @_;
-    
-    my @base_sets;
-    for my $code (keys %{$code_ref}) {
-        my %bases;
-        for my $aa (keys %{$code_ref->{$code}}) {
-            for my $codon (@{$code_ref->{$code}{$aa}}) {
-                for my $base (split '', $codon) {
-                    $bases{$base}++;
-                }
-            }
-        }
-        my @code_bases = sort keys %bases;
-        push @base_sets, \@code_bases;
+# Exercise I
+# Modify the subroutine from Lesson 10 exercise 2, which calculates
+# the mean of a list of numbers. Make it take an array reference
+# as input, instead of the array itself.
+sub mean {
+    my $nums = shift;
+    my $sum = 0;
+    foreach my $num (@{$nums}) {
+        $sum += $num;
     }
-    return @base_sets;
+    return $sum/@{$nums};
 }
+
+my @genome_mb = (100.3, 2700, 4.6, 3200, 12.1);
+print mean(\@genome_mb), "\n";
+
+
+
+# Question 2
+# Write a subroutine that takes two array references as input, which
+# are the same length (you could check that they are the same length!).
+# Have it return a reference to a hash, where the keys are the
+# elements of the first array, and the values of the hash are the
+# elements of the second array.
+sub zip {
+    my $arr1 = shift;
+    my $arr2 = shift;
+    die "Length mismatch!" unless (@{$arr1} == @{$arr2});
+    my %zipped;
+
+    for my $i (0..(@{$arr1} - 1)) {
+        $zipped{$arr1->[$i]} = $arr2->[$i];
+    }
+  
+    return \%zipped;
+}
+
+my @array1 = (1,2,3);
+my @array2 = qw/a b c/;  # A short way of making an array of strings!
+my $zip_ref = zip(\@array1, \@array2);
+print Dumper $zip_ref;
+
